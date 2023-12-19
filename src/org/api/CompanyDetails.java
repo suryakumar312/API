f814db0f91e66986fb4bd00768dac928282337e0
surya kumar <suryakumar0393@gmail.com>
2023-12-19 20:11:35 +0530

    updated

---
+++ src/org/api/CompanyDetails.java
@@ -0,0 +1,14 @@
+package org.api;
+
+public class CompanyDetails {
+	
+	private void comName() {
+
+		System.out.println("company name is TCS");
+	}
+	public static void main(String[] args) {
+		CompanyDetails c = new CompanyDetails();
+		c.comName();
+	}
+
+}
